5f6ce0e9d58ea23b4110631d5ff13384d0573a0a
Md Armaan <mdarmaan6204@gmail.com>
2025-02-09 17:54:12 +0530

    refactor: replace lazy loading with direct imports in index.js for improved readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { createContext, Suspense, useState } from "react";
+import { createContext, lazy, Suspense, useState } from "react";
 import {
   Header,
   Footer,
@@ -27,7 +27,7 @@ import {
   Photoinfo6,
   Photoinfo7,
   Photoinfo8,
-  Photoinfo9,
+  Photoinfo9, 
   ScanVideo,
   MScan,
   CusScan,
@@ -50,6 +50,11 @@ import {
   LoginForImage,
   MakeUp,
 } from "./pages/index.js";
+
+import Mscan from "./pages/Scan/MScan.jsx";
+import Ibran from "./pages/Cards/Newqrcardslist.jsx";
+// import Home from "./pages/Home.jsx";
+const CardSection = lazy((e) => import("./pages/Cards/CardMahalSection.jsx"));
 
 export const ThemeContext = createContext("light");
 const App = () => {
@@ -94,298 +99,52 @@ const App = () => {
                 <QRSection />
               </Suspense>
             }
-          />
-          <Route
+            />
+            <Route 
             path="/qrcards"
             element={
-              <Suspense fallback={<div>Loading QR Special Cards...</div>}>
-                <QrCards />
+              <Suspense fallback={<div>Loading QR Special Cards...</div>}> 
+              <QrCards />
               </Suspense>
             }
           />
-          <Route
-            path="/Photoinfo1"
-            element={
-              <Suspense fallback={<Loading />}>
-                {" "}
-                <Photoinfo1 />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/Photoinfo2"
-            element={
-              <Suspense fallback={<Loading />}>
-                {" "}
-                <Photoinfo2 />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/Photoinfo3"
-            element={
-              <Suspense fallback={<Loading />}>
-                {" "}
-                <Photoinfo3 />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/Photoinfo4"
-            element={
-              <Suspense fallback={<Loading />}>
-                {" "}
-                <Photoinfo4 />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/Photoinfo5"
-            element={
-              <Suspense fallback={<Loading />}>
-                {" "}
-                <Photoinfo5 />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/Photoinfo6"
-            element={
-              <Suspense fallback={<Loading />}>
-                {" "}
-                <Photoinfo6 />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/Photoinfo7"
-            element={
-              <Suspense fallback={<Loading />}>
-                {" "}
-                <Photoinfo7 />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/Photoinfo8"
-            element={
-              <Suspense fallback={<Loading />}>
-                {" "}
-                <Photoinfo8 />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/Photoinfo9"
-            element={
-              <Suspense fallback={<Loading />}>
-                {" "}
-                <Photoinfo9 />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/FireCrackers"
-            element={
-              <Suspense fallback={<Loading />}>
-                {" "}
-                <FireCrakers />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/memories"
-            element={
-              <Suspense fallback={<Loading />}>
-                {" "}
-                <Memories />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/Hotel"
-            element={
-              <Suspense fallback={<Loading />}>
-                {" "}
-                <Hotel />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/MScan"
-            element={
-              <Suspense fallback={<Loading />}>
-                {" "}
-                <MScan />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/Scan/:inviteId"
-            element={
-              <Suspense fallback={<Loading />}>
-                {" "}
-                <ScanVideo />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/register"
-            element={
-              <Suspense fallback={<Loading />}>
-                <Register />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <Suspense fallback={<Loading />}>
-                <Login />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/logout"
-            element={
-              <Suspense fallback={<Loading />}>
-                <Logout />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/service"
-            element={
-              <Suspense fallback={<Loading />}>
-                <Service />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/makeup"
-            element={
-              <Suspense fallback={<Loading />}>
-                <MakeUp />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/CateringMain"
-            element={
-              <Suspense fallback={<Loading />}>
-                <CateringMain />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/Catering"
-            element={
-              <Suspense fallback={<Loading />}>
-                <Catering />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/CateringSection"
-            element={
-              <Suspense fallback={<Loading />}>
-                <CateringSection />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/yourmenu"
-            element={
-              <Suspense fallback={<Loading />}>
-                <YourMenu />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/images"
-            element={
-              <Suspense fallback={<Loading />}>
-                <LoginForImage />
-              </Suspense>
-            }
-          />
+          <Route path="/Photoinfo1" element={<Suspense fallback={<Loading />}> <Photoinfo1 /></Suspense>} />
+          <Route path="/Photoinfo2" element={<Suspense fallback={<Loading />}> <Photoinfo2 /></Suspense>} />
+          <Route path="/Photoinfo3" element={<Suspense fallback={<Loading />}> <Photoinfo3 /></Suspense>} />
+          <Route path="/Photoinfo4" element={<Suspense fallback={<Loading />}> <Photoinfo4 /></Suspense>} />
+          <Route path="/Photoinfo5" element={<Suspense fallback={<Loading />}> <Photoinfo5 /></Suspense>} />
+          <Route path="/Photoinfo6" element={<Suspense fallback={<Loading />}> <Photoinfo6 /></Suspense>} />
+          <Route path="/Photoinfo7" element={<Suspense fallback={<Loading />}> <Photoinfo7 /></Suspense>} />
+          <Route path="/Photoinfo8" element={<Suspense fallback={<Loading />}> <Photoinfo8 /></Suspense>} />
+          <Route path="/Photoinfo9" element={<Suspense fallback={<Loading />}> <Photoinfo9 /></Suspense>} />
+          <Route path="/FireCrackers" element={<Suspense fallback={<Loading />}> <FireCrakers /></Suspense>} />
+          <Route path="/memories" element={<Suspense fallback={<Loading />}> <Memories /></Suspense>} />
+          <Route path="/Hotel" element={<Suspense fallback={<Loading />}> <Hotel /></Suspense>} />
+          <Route path="/MScan" element={<Suspense fallback={<Loading />}> <MScan /></Suspense>} />
+          <Route path="/Scan/:inviteId" element={<Suspense fallback={<Loading />}> <ScanVideo /></Suspense>} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/makeup" element={<MakeUp/>} />
+          <Route path="/CateringMain" element={<CateringMain/>}/>
+          <Route path="/Catering" element={<Catering/>}/>
+          <Route path="/CateringSection" element={<CateringSection/>}/>
+          <Route path="/yourmenu" element={<YourMenu/>}/>
+          <Route path="/images" element={<LoginForImage/>}/>
           {/* <Route path="/Photo/:1" element={<Photo/>} /> */}
-          <Route
-            path="/Cards"
-            element={
-              <Suspense fallback={<Loading />}>
-                <CardsPartners />
-              </Suspense>
-            }
-          />
-
+          <Route path="/Cards" element={<CardsPartners/>} />
           {/* <Route path="/:inviteId" element={<CusScan />} />  */}
-          <Route
-            path="/:inviteId"
-            element={
-              <Suspense fallback={<Loading />}>
-                <CusScan2 />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/Courier"
-            element={
-              <Suspense fallback={<Loading />}>
-                <Courier />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/Cars"
-            element={
-              <Suspense fallback={<Loading />}>
-                <Cars />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/CusAfter"
-            element={
-              <Suspense fallback={<Loading />}>
-                <CusAfter />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/admin"
-            element={
-              <Suspense fallback={<Loading />}>
-                <AdminLayout />
-              </Suspense>
-            }
-          >
-            <Route
-              path="users"
-              element={
-                <Suspense fallback={<Loading />}>
-                  <AdminUsers />
-                </Suspense>
-              }
-            />
-            <Route
-              path="contacts"
-              element={
-                <Suspense fallback={<Loading />}>
-                  <AdminContacts />
-                </Suspense>
-              }
-            />
-            <Route
-              path="users/:id/edit"
-              element={
-                <Suspense fallback={<Loading />}>
-                  <AdminUpdate />
-                </Suspense>
-              }
-            />
-          </Route>
+          <Route path="/:inviteId" element={<CusScan2 />} />
+          <Route path="/Courier" element={<Courier />} />
+          <Route path="/Cars" element={<Cars />} />
+          <Route path="/CusAfter"  element={<CusAfter/>} />
           <Route path="*" element={<Error />} />
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route path="users" element={<AdminUsers />} />
+            <Route path="contacts" element={<AdminContacts />} />
+            <Route path="users/:id/edit" element={<AdminUpdate />} />
+          </Route>
         </Routes>
         <Footer />
       </ThemeContext.Provider>
